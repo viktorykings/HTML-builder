cca2fd1cb96737e64cc8ad174ed3e7ed9fe58bd3
viktorykings <kingsviktory@gmail.com>
2022-11-08 00:41:20 +0300

    feat: add 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+
+const readableStream = fs.createReadStream(path.join(__dirname,  'text.txt'), 'utf-8');
+readableStream.on('data', chunk => {
+    console.log(chunk);
+})
